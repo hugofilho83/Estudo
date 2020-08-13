354bdb793a2e09e25079674b27a9629d0c2e8b51
hugo tavares filho <hugotavares83.dev@gmail.com>
2020-08-13 11:16:07 -0300

    add page success

---
+++ React/nlv/web/src/pages/Success/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Sucess() {
+  return <div></div>;
+}
+
+export default Sucess;
--- React/nlv/web/src/routes.tsx
+++ React/nlv/web/src/routes.tsx
@@ -2,15 +2,19 @@ import React from "react";
 
 import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import TeacherForm from "./pages/TeacherForm";
 import TeacherList from "./pages/TeacherList";
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Landing} />
-      <Route path="/study" component={TeacherList} />
-      <Route path="/give-classes" component={TeacherForm} />
+      <Route path="/" exact component={Login} />
+      <Route path="/landing" exact component={Landing} />
+      <Route path="/study" exact component={TeacherList} />
+      <Route path="/give-classes" exact component={TeacherForm} />
+      <Route path="/register" exact component={Register} />
     </BrowserRouter>
   );
 }
