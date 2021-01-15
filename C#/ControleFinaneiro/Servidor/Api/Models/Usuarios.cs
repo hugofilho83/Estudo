9d5fd30db28fda599c384364749a06b556b49d2a
hugotavaresinfnet <hugo.filho@al.infnet.edu.br>
2021-01-14 22:13:21 -0300

    add all

--- C#/ControleFinaneiro/Servidor/Api/Models/Usuarios.cs
+++ C#/ControleFinaneiro/Servidor/Api/Models/Usuarios.cs
@@ -4,12 +4,12 @@ using System.Text.Json.Serialization;
 
 namespace Api.Models
 {
-  public class Usuarios
-  {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Login { get; set; }
-    public string Senha { get; set; }
-    public bool Ativo { get; set; }
-  }
+    public class Usuarios
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+    }
 }
